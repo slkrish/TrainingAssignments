31a69af11fbd8de4ebfadb8ed6b83ab4b43e6915
Saikrishna Peraveni <s.peraveni@zensar.com>
2022-06-16 18:43:19 +0530

    CodeBase4

---
+++ C#/CodeBasedTest4/ASPWebform/ASPWebform/Exam.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Exam.aspx.cs" Inherits="ASPWebform.Exam" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+        </div>
+         List of Items available in Store :<asp:DropDownList ID="DropDownList1" runat="server" Height="16px" Width="207px" AutoPostBack="True" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+        </asp:DropDownList>
+        <br />
+        <asp:Image ID="Image1" runat="server" Height="400px" Width="700px" />
+        <p>
+           
+            Cost of selected item : <asp:Button ID="Price" runat="server" OnClick="Button1_Click1" style="margin-left: 43px" Text="Price" Width="119px" />
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        </p>
+    </form>
+</body>
+</html>
---
+++ C#/CodeBasedTest4/ASPWebform/ASPWebform/Exam.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASPWebform
+{
+    public partial class Exam : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                string[] str = new string[] { "Select", "NISSAN-GTR", "PORSCHE-CAYMAN", "DODGE-CHARGER", "HEADPHONES", "IPHONE13" };
+                for (int i = 0; i < str.Length; i++)
+                {
+                    DropDownList1.Items.Add(str[i]);
+                }
+            }
+        }
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string str = DropDownList1.Text;
+            Image1.ImageUrl = "~/Photos/" + str + ".jpg";
+        }
+        protected void Button1_Click1(object sender, EventArgs e)
+        {
+            TextBox1.Text = DropDownList1.SelectedIndex.ToString();
+            if (DropDownList1.Text == "NISSAN-GTR")
+            {
+                TextBox1.Text = "Rs. 40000000";
+            }
+            else if (DropDownList1.Text == "PORSCHE-CAYMAN")
+            {
+                TextBox1.Text = "Rs. 2220000";
+            }
+            else if (DropDownList1.Text == "DODGE-CHARGER")
+            {
+                TextBox1.Text = "Rs. 35000000";
+            }
+            else if (DropDownList1.Text == "HEADPHONES")
+            {
+                TextBox1.Text = "Rs. 18000";
+            }
+            else
+            {
+                TextBox1.Text = "Rs. 74000";
+            }
+        }
+    }
+}
